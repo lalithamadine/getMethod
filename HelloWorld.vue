9d26c7bc24c216813b5a6d0ac26ca248e525a7c1
lalithamadine <45647968+lalithamadine@users.noreply.github.com>
2018-12-26 15:35:01 +0530

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    {{states }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      states: {}
+    }
+  },
+  created () {
+    axios.get('http://roadtrippers.herokuapp.com/api/v1/states/ ')
+      .then(response => {
+        console.log(response)
+        this.states = response.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+}
+</script>
